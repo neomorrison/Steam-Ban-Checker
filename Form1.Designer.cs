6167a80560f9035680b047a6b40bd6da7b1d2471
Neo Morrison <44535532+neomorrison@users.noreply.github.com>
2023-02-14 19:00:02 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace banchecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SteamIdsTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SteamIdsTextBox
+            // 
+            this.SteamIdsTextBox.Font = new System.Drawing.Font("Arial", 12F);
+            this.SteamIdsTextBox.Location = new System.Drawing.Point(12, 12);
+            this.SteamIdsTextBox.Multiline = true;
+            this.SteamIdsTextBox.Name = "SteamIdsTextBox";
+            this.SteamIdsTextBox.Size = new System.Drawing.Size(424, 87);
+            this.SteamIdsTextBox.TabIndex = 1;
+            this.SteamIdsTextBox.Text = "Add Steam64 UIDs (seperate with ,)";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(213)))), ((int)(((byte)(224)))));
+            this.button2.Location = new System.Drawing.Point(442, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Watch User(s)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(213)))), ((int)(((byte)(224)))));
+            this.button3.Location = new System.Drawing.Point(442, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save User(s)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(442, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reset List";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textBox1.Location = new System.Drawing.Point(13, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "API KEY (found at steamcommunity.com/dev/apikey)";
+            this.textBox1.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(213)))), ((int)(((byte)(224)))));
+            this.button4.Location = new System.Drawing.Point(442, 106);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Save Key";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(26)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(556, 714);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.SteamIdsTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Steam Ban Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox SteamIdsTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace banchecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> steamIds = new List<string>();
+        String apiKey = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string json = File.ReadAllText("apikey.json");
+                apiKey = JsonConvert.DeserializeObject<string>(json);
+            }
+            catch (FileNotFoundException)
+            {
+                textBox1.Visible = true;
+                button4.Visible = true;
+                button2.Enabled = false;
+            }
+
+            try
+            {
+                // Read the saved Steam IDs from a file
+                string json = File.ReadAllText("steam_ids.json");
+
+                // Deserialize the JSON string to a List<string>
+                steamIds = JsonConvert.DeserializeObject<List<string>>(json);
+
+                // Display the saved Steam IDs in the SteamIdsTextBox
+                SteamIdsTextBox.Text = string.Join(",", steamIds);
+
+                button2.PerformClick();
+            }
+            catch (FileNotFoundException)
+            {
+                // If the file is not found, there are no saved Steam IDs
+                steamIds = new List<string>();
+            }
+
+
+        }
+
+        public async Task<bool> isBannedAsync(string steamid)
+        {
+            var playerBans = await GetPlayerBans(steamid);
+            if (playerBans.NumberOfGameBans > 0 || playerBans.NumberOfVACBans > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<PlayerBans> GetPlayerBans(string steamid)
+        {
+            using (var client = new HttpClient())
+            {
+                var url = $"https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key={apiKey}&steamids={steamid}";
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<PlayerBansResponse>(json);
+                    return data.PlayerBans[0];
+                }
+                else
+                {
+                    throw new Exception($"Failed to retrieve player bans for steamid {steamid}. Status code: {response.StatusCode}");
+                }
+            }
+        }
+
+        public class PlayerBansResponse
+        {
+            [JsonProperty("players")]
+            public PlayerBans[] PlayerBans { get; set; }
+        }
+
+        public class PlayerBans
+        {
+            [JsonProperty("SteamId")]
+            public string SteamId { get; set; }
+
+            [JsonProperty("CommunityBanned")]
+            public bool CommunityBanned { get; set; }
+
+            [JsonProperty("VACBanned")]
+            public bool VACBanned { get; set; }
+
+            [JsonProperty("NumberOfVACBans")]
+            public int NumberOfVACBans { get; set; }
+
+            [JsonProperty("DaysSinceLastBan")]
+            public int DaysSinceLastBan { get; set; }
+
+            [JsonProperty("EconomyBan")]
+            public string EconomyBan { get; set; }
+
+            [JsonProperty("GameBan")]
+            public int GameBan { get; set; }
+
+            [JsonProperty("NumberOfGameBans")]
+            public int NumberOfGameBans { get; set; }
+
+            [JsonProperty("BanCount")]
+            public int BanCount { get; set; }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            // Retrieve the SteamIDs entered in the text box and split them into an array
+            string steamIdsText = SteamIdsTextBox.Text;
+            string[] steamIds = steamIdsText.Split(',');
+            var x = 12;
+            var y = 143;
+            // Loop through each SteamID in the array and retrieve profile information and ban status
+            foreach (string steamId in steamIds)
+            {
+                // Retrieve profile information
+                PlayerSummary playerSummary = await GetPlayerSummary(steamId);
+
+                // Retrieve ban status
+                PlayerBans playerBans = await GetPlayerBans(steamId);
+
+                // Display profile picture and name in your program
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.ImageLocation = playerSummary.AvatarMedium;
+                Label nameLabel = new Label();
+                nameLabel.Text = playerSummary.PersonaName;
+
+                // Display ban status in your program
+                Label banStatusLabel = new Label();
+                if (await isBannedAsync(steamId))
+                {
+                    banStatusLabel.Text = "Banned";
+                    banStatusLabel.ForeColor = Color.Red;
+                }
+                else
+                {
+                    banStatusLabel.Text = "Unbanned";
+                    banStatusLabel.ForeColor = Color.Green;
+                }
+
+                // set location
+                pictureBox.Location = new Point(x, y);
+                pictureBox.Height = 80;
+                nameLabel.Location = new Point(x + pictureBox.Width + 10, y);
+                banStatusLabel.Location = new Point(x + pictureBox.Width + 10 + nameLabel.Width + 10, y);
+
+                // set properties
+                nameLabel.ForeColor = Color.White;
+                banStatusLabel.Font = new Font("Arial", 14, FontStyle.Bold);
+                nameLabel.Font = new Font("Arial", 14, FontStyle.Regular);
+                banStatusLabel.AutoSize = true;
+
+                // Add the controls to your form
+                this.Controls.Add(pictureBox);
+                this.Controls.Add(nameLabel);
+                this.Controls.Add(banStatusLabel);
+                y += 100;
+            }
+        }
+
+
+        public async Task<PlayerSummary> GetPlayerSummary(string steamId)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={apiKey}&steamids={steamId}");
+                var content = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<RootObject<PlayerSummary>>(content);
+                return result.Response.Players.FirstOrDefault();
+            }
+        }
+        public class RootObject<T>
+        {
+            public T Response { get; set; }
+        }
+
+
+        public class PlayerSummary
+        {
+            public List<PlayerSummary> Players { get; set; }
+            public string SteamId { get; set; }
+            public string PersonaName { get; set; }
+            public string ProfileUrl { get; set; }
+            public string Avatar { get; set; }
+            public string AvatarMedium { get; set; }
+            public string AvatarFull { get; set; }
+            public string PersonaState { get; set; }
+            public int CommunityVisibilityState { get; set; }
+            public int ProfileState { get; set; }
+            public string LastLogoff { get; set; }
+            public int CommentPermission { get; set; }
+            public string RealName { get; set; }
+            public string PrimaryClanId { get; set; }
+            public int TimeCreated { get; set; }
+            public int GameId { get; set; }
+            public string GameExtraInfo { get; set; }
+            public string GameServerIp { get; set; }
+            public string CityId { get; set; }
+            public string LocCountryCode { get; set; }
+            public string LocStateCode { get; set; }
+            public string LocCityId { get; set; }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Split the Steam IDs in the SteamIdsTextBox and add them to the list
+            steamIds = SteamIdsTextBox.Text.Split(',').Select(id => id.Trim()).ToList();
+
+            // Serialize the list to a JSON string and save it to a file
+            string json = JsonConvert.SerializeObject(steamIds);
+            File.WriteAllText("steam_ids.json", json);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete the stored user list?", "Caution", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                // Delete the stored user list file
+                File.Delete("steam_ids.json");
+
+                // Clear the SteamIDsTextBox
+                SteamIdsTextBox.Clear();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string input = JsonConvert.SerializeObject(textBox1.Text);
+            File.WriteAllText("apikey.json", input);
+            apiKey = textBox1.Text;
+            button2.Enabled = true;
+        }
+    }
+}
